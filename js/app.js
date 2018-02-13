e9a34a26a3e35500cdfff7625e9c7999349359d1
danielvportlandia <danielshelton90@gmail.com>
2018-02-13 08:50:29 -0800

    Still finalizing total value.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
-//how much cookies does each location need per day to make
-//hours of operation is 6am - 8pm
-
-//These next 3 values change store to store.
-// The minimum number of customers per hour.
-// The maximum number of customers per hour.
-// The average number of cookies purchased per customer.
-
-// the stores are Lower Queen Anne, Cascade, Pike Place Market, Capital Hill, Downtown
-
-//Return Math.floor(Math.random() * (max - min + 1)) + min;
-
-var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
 var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 var pike = {
@@ -20,11 +8,13 @@ var pike = {
   avgCookiesSold: 6.3,
   customersPerHour: [],
   cookiesPerHour: [],
+  dailyCookieTotal: 0,
+  results: [this.customersPerHour, this.cookiesPerHour],
   numCustomers: function() { //generates a random number of customers.
     return Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers;
   },
   getCustomersPerHour: function() { //fills the customersPerHour array.
-    for (var i = 0; i < hours.length; i++) {
+    for (var i = 0; i <= hours.length; i++) {
       this.customersPerHour.push(this.numCustomers());
     }
   },
@@ -32,14 +22,38 @@ var pike = {
     return Math.ceil(a * b);
   },
   getCookiesPerHour: function() { //generates cookies per hour.
-    for (var i = 0; i < hours.length; i++) {
+    for (var i = 0; i <= hours.length; i++) {
       this.cookiesPerHour.push(this.roundCookies(this.customersPerHour[i], this.avgCookiesSold));
     }
   },
-  render: [this.customersPerHour, this.cookiesPerHour],
+  getDailyCookieTotal: function() {
+    var value = 0;
+    this.getCookiesPerHour();
+    for (var i = 0; i <= hours.length; i++) {
+      value += this.cookiesPerHour[i];
+      console.log(value);
+    }
+    this.daileyCookieTotal = this.value;
+  },
+  render: function() {
+    this.getCustomersPerHour();
+    this.getCookiesPerHour();
+    this.getDailyCookieTotal();
+    var ulEl = document.getElementById('pike');
+    for (var i = 0; i <= hours.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hours[i] + this.cookiesPerHour[i];
+      ulEl.appendChild(liEl);
+    }
+    liEl.textContent = 'Total: ' + this.dailyCookieTotal;
+    console.log(this.dailyCookieTotal);
+    ulEl.appendChild(liEl);
+  },
 };
-pike.getCustomersPerHour();
-pike.getCookiesPerHour();
+// pike.getCustomersPerHour();
+// pike.getCookiesPerHour();
+// pike.getDailyCookieTotal();
+pike.render();
 // for (i = 0; i < hours.length; i++) {
 //totalCookies for the day method
 //cookiesSoldEachHour = [] method
